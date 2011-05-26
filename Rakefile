0f11b64f5de5c79997e6a04db332b23a97456a8f
clured <davidwilliammcclure@gmail.com>
2011-05-26 09:55:21 -0400

    Added new README.md file scraped from old wikis

--- Rakefile
+++ Rakefile
@@ -9,9 +9,30 @@ repos = %w[https://addons.omeka.org/svn/plugins/EadImporter/ https://addons.omek
 
 namespace :svn do
   desc 'List the repos to migrate to Github'
+
   task :list do
     repos.each do |repo|
       puts repo
     end
   end
+
+  task :migrate do
+    repos.each do |repo|
+      system "git svn clone -s " + repo
+      plugin_name = repo[37..-2]
+      system "ruby readme_scrape.rb " + plugin_name
+      system "mv README.md " + plugin_name + "/README.md"
+      system "cd " + plugin_name
+      github_account_name = "clured"
+      github_account_api_token = "bc0c6979cd69c80f7f7d9a13f6e35758"
+      system "curl -F 'login=" + github_account_name + "' -F 'token=" + github_account_api_token + "' https://github.com/api/v2/yaml/repos/create -F name=" + plugin_name
+      system "git remote add origin git@github.com:" + github_account_name + "/" + plugin_name + ".git"
+      system "git add ."
+      system "git commit -m 'Added new README.md file scraped from old wikis'"
+      system "git push origin master"
+      system "cd .."
+      system "python convert.py " + github_account_name + " " + plugin_name + " " + github_account_api_token
+    end
+  end
+
 end
---
+++ convert.py
@@ -0,0 +1,70 @@
+# Takes three parameters from the cli - github username, repo name, and api
+# token
+import sys
+import csv
+import json
+import urllib
+
+USERNAME = sys.argv[1]
+ORGNAME = ''
+PROJECT = sys.argv[2]
+AUTH_TOKEN = sys.argv[3]
+TRAC_URL = 'https://addons.omeka.org/trac/report/1?format=csv'
+
+print USERNAME, PROJECT, AUTH_TOKEN
+
+if (ORGNAME == ''):
+    ORGNAME = USERNAME
+
+github_url = 'https://github.com/api/v2/json/issues/'
+csv_data = urllib.urlopen(TRAC_URL)
+reader = csv.DictReader(csv_data)
+tickets = []
+
+url = github_url + 'list/%s/%s/open' % (ORGNAME, PROJECT)
+response = urllib.urlopen(url)
+content = response.read()
+issues = json.loads(content)['issues']
+
+for row in reader:
+    if row['component'] == PROJECT:
+        for key, value in row.items():
+            row[key] = row[key].decode('utf-8')
+
+        if filter(lambda i: i['title'] == row['summary'], issues):
+            continue
+
+        tickets.append({
+            'title': row['summary'],
+            'description': row['_description'],
+            'tags': [u'ime', row['type'], row['component']],
+        })
+
+for ticket in tickets:
+    url = github_url + 'open/%s/%s' % (ORGNAME, PROJECT)
+    data = urllib.urlencode({
+        'login': USERNAME,
+        'token': AUTH_TOKEN,
+        'title': ticket['title'],
+        'body': ticket['description'],
+    })
+
+    urllib.urlopen(github_url, data)
+    response = urllib.urlopen(url, data)
+    content = response.read()
+
+    try:
+        issue = json.loads(content)['issue']
+    except KeyError:
+        raise Exception(content)
+
+    data = urllib.urlencode({
+        'login': USERNAME,
+        'token': AUTH_TOKEN,
+    })
+
+    for tag in ticket['tags']:
+        url = github_url + 'label/add/%s/%s/%s/%s' % (
+            ORGNAME, PROJECT, tag, issue['number'])
+        urllib.urlopen(url, data)
+
